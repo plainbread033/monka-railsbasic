2bde9ef775b377143afeaa676621404a921778ef
plainbread033 <royal.plainbread@gmail.com>
2019-12-19 15:16:47 +0900

    ルートのパスを変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_042423) do
+ActiveRecord::Schema.define(version: 2019_12_19_055423) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "published_on"
+    t.integer "price"
+    t.boolean "displayed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
